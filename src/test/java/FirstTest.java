f2e8b24492abef6dc0f13ec12a9c630f7f57a23d
smitkadam0107 <smitkadam0107@gmail.com>
2025-11-28 20:09:47 +0530

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class FirstTest {
+
+    @Test
+    public void testUsersAPI() {
+        given()
+                .baseUri("https://jsonplaceholder.typicode.com")
+        .when()
+                .get("/users/1")
+       .then()
+                .statusCode(200)
+                .body("address.zipcode",equalTo( "92998-3874"));
+
+    }
+
+}
